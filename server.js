35e7721e89f7c8db521af566bd42719f885eb552
Max Plumer <68719068+mplumer@users.noreply.github.com>
2020-10-12 20:36:10 -0400

    set default port

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const { animals } = require('./data/animals.json');
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 
@@ -63,6 +64,6 @@ app.get('/api/animals', (req, res) => {
 });
 
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
-});
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+  });
